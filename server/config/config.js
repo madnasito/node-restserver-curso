f461ea62c02dd7475302826ba2ab0a381c2fd1f3
unknown <madnamv@gmail.com>
2020-09-16 10:13:23 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:contraseña@cafe.4osf9.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
