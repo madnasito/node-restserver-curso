c28179146d8c7e05eff536dd8ab20bd7c4f2564f
Manuel <49660111+madnasito@users.noreply.github.com>
2020-09-17 13:03:14 -0400

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -15,18 +15,15 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 //Base de datos
 let urlDB;
 
-<<<<<<< HEAD
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = process.env.MONGO_URI;
 }
-=======
  if (process.env.NODE_ENV === 'dev') {
      urlDB = 'mongodb://localhost:27017/cafe';
  } else {
     urlDB = process.env.MONGO_URI;
  }
->>>>>>> ec0880f11df055bc2fd25aa164af7a7269a7143c
 
 process.env.urlDB = urlDB;
